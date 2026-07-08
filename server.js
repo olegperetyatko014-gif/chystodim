effa16667f2795a143ef5d534bb7a3fda03f1ff6
olegperetyatko014-gif <olegperetyatko014@gmail.com>
2026-07-08 12:57:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,27 @@ app.post("/order", async (req, res) => {
     try {
 
         const chatIds = [
-    "1987865626",
-    "7204226375"
-];
+            "1987865626",
+            "7204226375"
+        ];
 
-for (const chatId of chatIds) {
-    const response = await fetch(
-        `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                chat_id: chatId,
-                text: text
-            })
+        for (const chatId of chatIds) {
+            await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: text
+                })
+            });
         }
-    );
 
-    console.log(await response.text());
-}
+        res.json({ success: true });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false });
+    }
+});
